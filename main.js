c53317efc74f876e03d48b7edf60fd502ad81e3e
Gameboxapp <casjavy@gmail.com>
2024-05-28 17:44:53 -0600

    Update main.js

--- main.js
+++ main.js
@@ -4,22 +4,32 @@ let navbar = document.querySelector('.navbar');
 menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
-}
+};
 
-window.onscroll = () => {
-    if (menu.classList.contains('fa-times')) {
-        menu.classList.remove('fa-times');
-        navbar.classList.remove('active');
-    }
-}
+// No need to toggle on window scroll, so removed that part
 
 document.querySelector('#search-icon').onclick = () => {
     document.querySelector('#search-form').classList.toggle('active');
-}
+};
 
-document.querySelector('#close').onclick = () => {
+document.querySelector('#close').onclick = () =>{
     document.querySelector('#search-form').classList.remove('active');
 }
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 9500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true,
+  });
+
  
 
 
